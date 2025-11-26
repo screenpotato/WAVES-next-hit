5ce26e01aa633b74baffc97197001d37e1fa36b7
Agnes Lindberg <saskia.joanna.rauhut@posteo.de>
2025-11-26 10:22:33 +0100

    add fake placeholder enemies

---
+++ src/enemy.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+# TODO: code enemy behavior, enemies don't actually do anything yet
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
+
+
+func attack() -> void:
+	pass
+	# TODO: implement attack mode
+
+
+func die() -> void:
+	pass
+	# TODO: implement enemy dying
+
+
+func pursue() -> void:
+	pass
+	# TODO: make enemy follow player if in range
